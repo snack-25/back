110ae1d60fac90554410adfbc457a5a8b0661a8f
dani784601 <139238536+dani784601@users.noreply.github.com>
2025-03-17 13:45:44 +0900

    ✨ feat(api): 카테고리 조회 기능 추가(all,parent,sub)

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,11 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
+import { Category } from '@prisma/client';
 
 @Controller('categories')
 export class CategoriesController {
   public constructor(private readonly categoriesService: CategoriesService) {}
 
   // TODO: /categories (GET) 카테고리 목록 조회
+  @Get('')
+  public findAll(): Promise<Category[]> {
+    return this.categoriesService.findAll();
+  }
+
+  @Get('/parents')
+  public getParentCategories(): Promise<Category[]> {
+    return this.categoriesService.getParentCategories();
+  }
+
+  @Get('/:id')
+  public getSubCategories(@Param('id') id: string): Promise<Category[]> {
+    return this.categoriesService.getSubCategories(id);
+  }
+
   // TODO: /categories (POST) 카테고리 추가
   // TODO: /categories/{categoryId} (PUT/PATCH) 카테고리 수정
   // TODO: /categories/{categoryId} (DELETE) 카테고리 삭제
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { Category, PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  public async findAll(): Promise<Category[]> {
+    const categories = await prisma.category.findMany({
+      where: {
+        isActive: true,
+      },
+    });
+    return categories;
+  }
+
+  public async getParentCategories(): Promise<Category[]> {
+    const categories = await prisma.category.findMany({
+      where: {
+        parentId: null,
+        isActive: true,
+      },
+    });
+    return categories;
+  }
+
+  public async getSubCategories(id: string): Promise<Category[]> {
+    const categories = await prisma.category.findMany({
+      where: {
+        parentId: id,
+        isActive: true,
+      },
+    });
+    return categories;
+  }
+}
