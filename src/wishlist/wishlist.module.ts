91f8d3f57de7ed35a32d0b329631e42bd29d60fe
soohwanpak <qoiuytnghks4@gmail.com>
2025-03-20 13:15:04 +0900

    :bug: fix(api) : wishlist_module prismamodule설정 추가

--- src/wishlist/wishlist.module.ts
+++ src/wishlist/wishlist.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WishlistService } from './wishlist.service';
 import { WishlistController } from './wishlist.controller';
+import { PrismaModule } from '../shared/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [WishlistController],
   providers: [WishlistService],
 })
