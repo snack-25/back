23226129bf869f39931e397b3915d0a849f872f5
soohwanpak <qoiuytnghks4@gmail.com>
2025-03-20 13:05:28 +0900

    :tada: feat(api) : 찜하기, 찜 취소, 찜목록조회 API 작성

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,6 +13,7 @@ import { OrdersModule } from './orders/orders.module';
 import { OrderRequestsModule } from './order-requests/order-requests.module';
 import { BudgetsModule } from './budgets/budgets.module';
 import { InvitationsModule } from './invitations/invitations.module';
+import { WishlistModule } from './wishlist/wishlist.module';
 
 @Module({
   imports: [
@@ -33,6 +34,7 @@ import { InvitationsModule } from './invitations/invitations.module';
     OrderRequestsModule,
     BudgetsModule,
     InvitationsModule,
+    WishlistModule,
   ],
   controllers: [AppController],
   providers: [AppService],
