aef5ff912b54bcf88755fadc3918723cbda77dfc
dani784601 <139238536+dani784601@users.noreply.github.com>
2025-03-24 09:46:07 +0900

    refactor: class-validator 추가

--- src/companies/dto/company.response.dto.ts
+++ src/companies/dto/company.response.dto.ts
@@ -1,22 +1,26 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsDate, IsOptional } from 'class-validator';
 
 export class CompanyResponseDto {
   @ApiProperty({
     description: '기업 ID',
     example: 'kmucb8i7durz6je49lpjj6un',
   })
+  @IsString()
   public id: string;
 
   @ApiProperty({
     description: '기업명',
     example: '스낵25',
   })
+  @IsString()
   public name: string;
 
   @ApiProperty({
     description: '사업자 등록번호',
     example: '1234567890',
   })
+  @IsString()
   public bizno: string;
 
   @ApiProperty({
@@ -24,6 +28,8 @@ export class CompanyResponseDto {
     example: '서울특별시 강남구 테헤란로 123',
     required: false,
   })
+  @IsString()
+  @IsOptional()
   public address?: string;
 
   @ApiProperty({
@@ -31,17 +37,21 @@ export class CompanyResponseDto {
     example: '12345',
     required: false,
   })
-  public zipcode: string;
+  @IsString()
+  @IsOptional()
+  public zipcode?: string;
 
   @ApiProperty({
     description: '생성 일시',
     example: '2023-01-01T00:00:00.000Z',
   })
+  @IsDate()
   public createdAt: Date;
 
   @ApiProperty({
     description: '수정 일시',
     example: '2023-01-01T00:00:00.000Z',
   })
+  @IsDate()
   public updatedAt: Date;
 }
