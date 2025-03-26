4b532b7228e406b97e816933966b2315793d7c7f
unknown <lyj69641@gmail.com>
2025-03-26 17:14:08 +0900

    🔥 Dto 처리일 삭제

--- src/order-requests/dto/reject-order-request.dto.ts
+++ src/order-requests/dto/reject-order-request.dto.ts
@@ -9,8 +9,4 @@ export class RejectOrderRequestDto {
   @ApiProperty({ description: '거절 메시지', example: '예산 부족으로 인해 거절되었습니다.' })
   @IsString()
   notes: string;
-
-  @ApiProperty({ description: '주문 요청 거절 시각', example: '2025-03-12T15:30:00Z' })
-  @IsDate()
-  resolvedAt: Date = new Date();
 }
